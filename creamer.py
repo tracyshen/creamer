0f6d248e51489deec33161f5c4513f934a8c85ad
vitrun <vitrun@ffe68fe6-ea00-11de-a198-37193048d1ed>
2009-12-16 06:56:11 +0000

    alex

--- creamer.py
+++ creamer.py
@@ -1,14 +1,23 @@
 #coding:utf-8
 from urllib import urlopen
-import pageparser,datamgr
+import pageparser,datamgr,sys
 
-url='http://house.focus.cn/showarticle/1911/572831.html'
-rawContent=datamgr.to_utf8(urlopen(url).read())
-pageparser=pageparser.CreamParser()	
-pageparser.feed(rawContent)
-#~ print 'url: ',url
-#~ print 'title: ',pageparser.spot.title
-#~ print 'keywords: ',pageparser.spot.keywords
-#~ print 'body data: ',pageparser.bdata
-pageparser.get_cream()
-#~ print pageparser.cream
+
+def get_cream(url):
+	rawContent=datamgr.to_utf8(urlopen(url).read())
+	pparser=pageparser.CreamParser()	
+	pparser.feed(rawContent)
+	#~ print 'url: ',url
+	#~ print 'title: ',pageparser.spot.title
+	#~ print 'keywords: ',pageparser.spot.keywords
+	#~ print 'body data: ',pageparser.bdata
+	pparser.get_cream()
+	#~ print pageparser.cream
+	
+
+if __name__=='__main__':
+	if len(sys.argv)>1:
+		get_cream(sys.argv[1])
+	else:
+		url='http://house.focus.cn/showarticle/1911/572831.html'
+		get_cream(url)
